5e2bbe7d9253e32d556a4b2919968f32d8898c20
pavan <kumarpavan14341@gmail.com>
2021-07-14 16:37:04 +0000

    added gitignore file

---
+++ Readme.txt
@@ -0,0 +1,6 @@
+1.steps to run mysqlbackup.sh
+	-create a cred file where you want to run the script.
+		ex- cat cred
+		    username=dbuser
+		    password=welcome1
+2. run the script with DB  name as argument to it.
--- mysqlbackup.sh
+++ mysqlbackup.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
-
-mysqldump -h db_host.rds.aws.com -u dbuser -p welcome1 db_flipkart > /hbackup/db_flipkart.sql
+v_username=$( cat cred | grep username|awk -F"=" '{print $2}')
+v_password=$( cat cred | grep password|awk -F"=" '{print $2}')
+v_dbHost=db_host.rds.aws.com
+v_dbName=$1
+echo "mysqldump -h $v_dbHost -u $v_username -p $v_password $v_dbName > /hbackup/$v_dbName.sql"
